4f27dfbd6662df5f7b17c979e5d600f990143fcf
chrispetito <christopherpetito@gmail.com>
2019-10-16 11:26:11 -0700

    move operands into while loop

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,6 +4,7 @@ import sys
 HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
+MUL = 0b10100010 
 
 print('argv',sys.argv)
 
@@ -13,7 +14,7 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 256
-        self.registers = [0] * 8
+        self.reg = [0] * 8
         self.pc = 0
 
     def load(self, filename):
@@ -33,37 +34,21 @@ class CPU:
                     except ValueError:
                         continue
                     # write into ram
-                    self.ram_write(address, num)
+                    # print('address:', address, 'num:', num)
+                    self.ram_write(address, val)
                     address += 1
 
         except FileNotFoundError:
-            print('not found')
-            sys.exit(2)
-
-        # address = 0
-
-        # # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
+            print('File not found')
+            sys.exit(1)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -90,24 +75,27 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
         is_running = True
         while is_running is True:
-            cmd = self.ram[self.pc]
-
-            if cmd == LDI:
-                self.registers[operand_a] = operand_b
+            ir = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            if ir == LDI:
+                self.reg[operand_a] = operand_b
                 self.pc += 3
-            elif cmd == PRN:
-                print(self.registers[operand_a])
-                # print(self.registers)
+            elif ir == PRN:
+                print(self.reg[operand_a])
+                # print(self.reg)
                 self.pc += 2
-            elif cmd == HLT:
+            elif ir == HLT:
                 is_running = False
                 sys.exit(1)
+            elif ir == MUL:
+                self.alu('MUL', operand_a, operand_b)
+                # self.reg[operand_a] *= self.reg[operand_b]
+                self.pc += 3
             else:
-                print(f'Unknown command: {cmd}')
+                print(f'Unknown command: {ir}')
                 sys.exit(1)
 
     def ram_read(self, mar):
